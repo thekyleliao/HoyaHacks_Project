28fd3b3294d748cc9a0eb7a200305ac7b3f6c84f
stati30241 <staticsahib30241@gmail.com>
2024-01-27 11:01:57 -0500

    Working

--- main.py
+++ main.py
@@ -1,41 +1,26 @@
-# Imports stuff
-import taipy as tp
-from taipy import Config, Core, Gui
+from taipy.gui import Gui, notify
 
+text = "Original text"
 
-# Function that builds a message from input
-def build_message(age):
-    return f"You are {age} years old!"
-
-
-# Config stuff
-input_name_data_node_cfg = Config.configure_data_node(id="input_name")
-message_data_node_cfg = Config.configure_data_node(id="age")
-build_msg_task_cfg = Config.configure_task("build_msg", build_message, input_name_data_node_cfg, message_data_node_cfg)
-scenario_cfg = Config.configure_scenario("scenario", task_configs=[build_msg_task_cfg])
-
-# Variables
-input_name = "18"
-age = None
-
-
-# no clue what this does
-def submit_scenario(state):
-    state.scenario.input_name.write(state.input_name)
-    state.scenario.submit()
-    state.age = scenario.age.read()
-
-# Formats the page
+# Definition of the page
 page = """
-Enter your age: <|{input_name}|input|>
+# Getting started with Taipy GUI
 
-<|Click|button|on_action=submit_scenario|>
+My text: <|{text}|>
 
-Message: <|{age}|text|>
+<|{text}|input|>
+
+<|Run local|button|on_action=on_button_action|>
 """
 
-# Runs the application
-if __name__ == "__main__":
-    Core().run()
-    scenario = tp.create_scenario(scenario_cfg)
-    Gui(page).run()
+def on_button_action(state):
+    notify(state, 'info', f'The text is: {state.text}')
+    state.text = "Button Pressed"
+
+def on_change(state, var_name, var_value):
+    if var_name == "text" and var_value == "Reset":
+        state.text = ""
+        return
+
+
+Gui(page).run()
