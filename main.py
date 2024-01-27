64520d08e41eacdac941364e87f0884e38f71176
stati30241 <staticsahib30241@gmail.com>
2024-01-27 12:23:56 -0500

    Upload and display images

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from taipy.gui import Gui, notify
+import pandas as pd
+from PIL import Image
+from io import BytesIO
 
-text = "Original text"
+original_image = None
+content = None
 
 # Definition of the page
 page = """
 # Getting started with Taipy GUI
 
-My text: <|{text}|>
+<|{content}|file_selector|label=Select Image|extensions=.png,.jpg|on_action=on_file_upload|>
 
-<|{text}|input|>
-
-<|Run local|button|on_action=on_button_action|>
+<|{original_image}|image|>
 """
 
 def on_button_action(state):
@@ -21,6 +23,12 @@ def on_change(state, var_name, var_value):
     if var_name == "text" and var_value == "Reset":
         state.text = ""
         return
+
+def on_file_upload(state):
+    image = Image.open(state.content)
+    buf = BytesIO()
+    image.save(buf, format="PNG")
+    state.original_image = buf.getvalue()
 
 
 Gui(page).run()
